82dec91154206c7fc4bd28bba0aa0d79b3f6c8fc
jeronimoek <jeronimoek@gmail.com>
2023-12-25 22:46:25 -0300

    fix: update dependency, fix UI issues

---
+++ src/components/ColorSlider.tsx
@@ -0,0 +1,107 @@
+import ColorTranslator, { Color } from 'color-translate'
+import { useMemo } from 'react'
+import './ColorSlider.scss'
+
+function propToPercentage<T extends keyof ColorTranslator>(
+  number: number,
+  format: T,
+  prop: T[number],
+) {
+  if (format === 'rgb') {
+    return number / 255
+  } else if (
+    (format === 'hsl' || format === 'hwb' || format === 'lch') &&
+    prop === 'h'
+  ) {
+    return number / 360
+  } else if ((format === 'lab' || format === 'lch') && prop === 'l') {
+    return number / 100
+  } else if (format === 'lab' && (prop === 'a' || prop === 'b')) {
+    return (number / 125 + 1) / 2
+  } else if (format === 'lch' && prop === 'c') {
+    return number / 150
+  }
+
+  return number
+}
+
+export function ColorSlider<T extends keyof ColorTranslator>({
+  color,
+  format,
+  prop,
+  stepsNum = 7,
+  onClick,
+}: Readonly<{
+  color: ColorTranslator
+  format: T
+  prop: T[number]
+  stepsNum?: number
+  onClick: (percentage: number) => any
+}>) {
+  const background = useMemo(() => {
+    const steps = []
+
+    for (let i = 0; i < stepsNum; i++) {
+      let step: number
+      if (format === 'lab' && (prop === 'a' || prop === 'b')) {
+        step = (i / (stepsNum - 1)) * 200 - 100
+      } else {
+        step = (i / (stepsNum - 1)) * 100
+      }
+
+      steps.push(
+        (
+          new ColorTranslator({
+            ...(color[format] as Color),
+            [prop]: `${step}%`,
+          })[format] as { toString: () => string }
+        ).toString(),
+      )
+    }
+
+    return `linear-gradient(90deg, ${steps.join(', ')})`
+  }, [color])
+
+  const propValue = (color[format] as Color)[prop as keyof Color] as number
+  const propPercentage = propToPercentage(propValue, format, prop)
+
+  return (
+    <div
+      style={{
+        background,
+      }}
+      className="color-slider"
+      onMouseDown={ev => {
+        function mousemoveHandler(e: MouseEvent) {
+          const rect = (ev.target as Element).getBoundingClientRect()
+          const x = e.clientX - rect.left
+          if (x < 0 || x > rect.width) return
+          let percentage = x / rect.width
+          if (format === 'lab' && (prop === 'a' || prop === 'b')) {
+            percentage = percentage * 2 - 1
+          }
+          onClick(percentage)
+        }
+
+        function mousemoveRemove() {
+          document.removeEventListener('mousemove', mousemoveHandler)
+          document.removeEventListener('mouseup', mousemoveRemove)
+        }
+
+        mousemoveHandler(ev as any)
+
+        document.addEventListener('mousemove', mousemoveHandler)
+        document.addEventListener('mouseup', mousemoveRemove)
+      }}
+    >
+      <div
+        className="picker-container"
+        style={{
+          width: `calc(100% - ${propPercentage} * 100%)`,
+        }}
+      >
+        <div className="picker" />
+      </div>
+    </div>
+  )
+}
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,7 @@
-import { Button, Form, Input } from 'antd'
+import { useMemo, useState } from 'react'
 import './Home.scss'
+import { ColorSlider } from 'components/ColorSlider'
+import ColorTranslator, { Color } from 'color-translate'
 
 export interface ITask {
   id: number
@@ -11,30 +13,61 @@ export interface ITask {
 }
 
 export function Home() {
-  const [form] = Form.useForm()
+  const [color, setColor] = useState<ColorTranslator>(
+    new ColorTranslator({
+      r: 255,
+      g: 255,
+      b: 123,
+      alpha: 1,
+    }),
+  )
+
+  const formats = [
+    'rgb',
+    'hsl',
+    // 'hwb',
+    'lab',
+    'lch',
+  ] as const
+
+  function onClick<T extends keyof ColorTranslator>(
+    percentage: number,
+    format: T,
+    prop: T[number],
+  ) {
+    setColor(
+      new ColorTranslator({
+        ...(color[format] as Color),
+        [prop]: `${percentage * 100}%`,
+      }),
+    )
+  }
+
+  const sliders = useMemo(() => {
+    return formats.map(format => (
+      <div key={format} className="format">
+        <h4>{color[format].toString()}</h4>
+        {[...format.split('')].map(prop => (
+          <div key={prop} className="prop">
+            <ColorSlider
+              onClick={percentage => {
+                onClick(percentage, format, prop)
+              }}
+              color={color}
+              format={format}
+              prop={prop}
+            />
+          </div>
+        ))}
+      </div>
+    ))
+  }, [color, setColor])
 
   return (
     <div className="home">
       <div className="home_container">
-        <h1>My Tasks</h1>
-        <div>
-          <Form
-            form={form}
-            onFinish={async values => {
-              console.log(values)
-              form.resetFields()
-            }}
-          >
-            <div className="add-form">
-              <Button type="primary" htmlType="submit">
-                Add
-              </Button>
-              <Form.Item name="title">
-                <Input placeholder="Task title" />
-              </Form.Item>
-            </div>
-          </Form>
-        </div>
+        <h1>My Formats</h1>
+        <div>{sliders}</div>
       </div>
     </div>
   )
